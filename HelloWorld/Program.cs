8e63e2c9a76d2af1e2132ac00eb09827b412b6b6
Bisma <Bisma@DESKTOP-CU8VKG3>
2019-01-15 17:45:36 -0500

    Hello wolrd version 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // pause to display Hello World! 
+            Console.WriteLine("Press Any Key to Continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
